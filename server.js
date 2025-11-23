a90eff6226e840e5a2395c728d709ef6647733ef
kaviavarthini <kavivarthini@gmail.com>
2025-11-23 21:46:05 +0530

    server

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
-const path = require("path");
 const Image = require(path.join(__dirname, "models", "Image"));
 const Product = require(path.join(__dirname, "models", "Product"));
 const app = express();
